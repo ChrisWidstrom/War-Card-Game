58962694cc5cf6344ff546e60cc0208be6bb30e1
Chris Widstrom <cwidstrom@me.com>
2021-02-20 14:41:15 -0500

    Initial Commit

---
+++ war-challenge/war_challengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challengeApp.swift
+//  war-challenge
+//
+//  Created by Christoffer Widstrom on 2/20/21.
+//
+
+import SwiftUI
+
+@main
+struct war_challengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
